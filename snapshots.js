23ef0dcb140dcbbf09d4725374b3d57e9328b372
Tomasz S <tom.sobczyk@gmail.com>
2020-11-22 00:13:13 +0100

    Update snapshots.js

--- snapshots.js
+++ snapshots.js
@@ -1,5 +1,6 @@
 const PercyScript = require('@percy/script');
 const MAIN_URL = 'https://www.snstudio.pl/';
+const COOKIE_BUTTON = ".ba-lightbox .ba-btn-transition";
 var myMap = new Map();
 myMap.set("Homepage", MAIN_URL);
 myMap.set("Sklep", MAIN_URL + "sklep");
@@ -10,6 +11,8 @@ myMap.forEach(function(url,key){
   PercyScript.run(async (page, percySnapshot) => {
  
     await page.goto(url);
+    page.click(COOKIE_BUTTON)
+    ".ba-lightbox .ba-btn-transition")
     page.evaluate(_ => {
       window.scrollBy(0, window.innerHeight);
     });
