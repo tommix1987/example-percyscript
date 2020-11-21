89ef7f0a3b9c4b0c99d490cd25f8507cdb65af17
Tomasz S <tom.sobczyk@gmail.com>
2020-11-21 22:46:09 +0100

    Update snapshots.js

--- snapshots.js
+++ snapshots.js
@@ -1,22 +1,15 @@
 const PercyScript = require('@percy/script');
-const httpServer = require('http-server');
-
-const PORT = process.env.PORT_NUMBER || 8000;
-const TEST_URL = `http://localhost:${PORT}`;
+const MAIN_URL = 'https://www.snstudio.pl;
+const widths = { widths: [768, 992, 1200] };
 
 // A script to navigate our app and take snapshots with Percy.
 PercyScript.run(async (page, percySnapshot) => {
-  let server = httpServer.createServer();
-  server.listen(PORT);
 
-  console.log(`Server started at ${TEST_URL}`);
-
-  await page.goto(TEST_URL);
-  await percySnapshot('TodoMVC home page');
+  await page.goto(MAIN_URL);
+  await percySnapshot('Homepage');
 
   // Enter a new to-do.
   await page.type('.new-todo', 'A really important todo');
   await page.keyboard.press('Enter');
-  await percySnapshot('TodoMVC with a new todo', { widths: [768, 992, 1200] });
-  server.close();
+  await percySnapshot('TodoMVC with a new todo', widths);
 });
