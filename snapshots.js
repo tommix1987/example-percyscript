eeb904560e65bd4d4a8417ff4eea0bba7575d6ae
Tomasz S <tom.sobczyk@gmail.com>
2020-11-21 23:48:43 +0100

    Update snapshots.js

--- snapshots.js
+++ snapshots.js
@@ -3,8 +3,8 @@ const MAIN_URL = 'https://www.snstudio.pl/';
 var myMap = new Map();
 myMap.set("Homepage", MAIN_URL);
 myMap.set("Sklep", MAIN_URL + "sklep");
-myMap.set("Promocje, MAIN_URL + "promocje");
-myMap.set("Produkt, MAIN_URL + "sklep/lakiery-hybrydowe/produkt/protein-base-gold-edition-7ml/category_pathway-92");
+myMap.set("Promocje", MAIN_URL + "promocje");
+myMap.set("Produkt", MAIN_URL + "sklep/lakiery-hybrydowe/produkt/protein-base-gold-edition-7ml/category_pathway-92");
 // A script to navigate our app and take snapshots with Percy.
 PercyScript.run(async (page, percySnapshot) => {
   myMap.forEach(function(value,key,map){
